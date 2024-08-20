af88fba590c5319c9d7eb86935607ff0c2e00aef
Apurva Ojas <apurvaojas10@gmail.com>
2024-08-20 14:28:00 +0530

    ci: :bug: fixed pipelines and package json meta data

--- packages/angular/src/lib/breakpoint-observer/breakpointObserver.directive.spec.ts
+++ packages/angular/src/lib/breakpoint-observer/breakpointObserver.directive.spec.ts
@@ -1,74 +1,123 @@
+/* eslint-disable @typescript-eslint/ban-types */
 import { Component } from '@angular/core';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
+import { TestBed } from '@angular/core/testing';
 import {
   BreakpointService,
   onBreakPointChangeCallbackArgs,
 } from '@breakpoint-observer/core';
 import { BreakpointObserverDirective } from './breakpointObserver.directive';
 
+// Mock BreakpointService
+jest.mock('@breakpoint-observer/core', () => ({
+  BreakpointService: class {
+    public breakpoints = {
+      sm: { min: 0, max: 599 },
+      md: { min: 600, max: 959 },
+      lg: { min: 960, max: 1279 },
+      xl: { min: 1280, max: Infinity },
+    };
+
+    public activeBreakpoint = {
+      breakpoint: 'sm',
+      maxWidth: 599,
+      minWidth: 0,
+    };
+
+    constructor() {
+      //
+    }
+
+    private listeners: { [key: string]: Function[] } = {};
+
+    on(event: string, listener: Function) {
+      if (!this.listeners[event]) {
+        this.listeners[event] = [];
+      }
+      this.listeners[event].push(listener);
+      this.emit(event, this.activeBreakpoint, this.breakpoints);
+    }
+
+    updateBreakpoint(breakpoint: keyof typeof this.breakpoints) {
+      this.activeBreakpoint = {
+        breakpoint,
+        maxWidth: this.breakpoints[breakpoint].max,
+        minWidth: this.breakpoints[breakpoint].min,
+      };
+      this.emit('onBreakPointChange', this.activeBreakpoint, this.breakpoints);
+    }
+
+    emit(event: string, ...args: any[]) {
+      if (this.listeners[event]) {
+        this.listeners[event].forEach((listener) => listener(...args));
+      }
+    }
+
+    static getInstance() {
+      return new this();
+    }
+  },
+}));
+
 @Component({
-  template: `<div libBreakpointObserver></div>`,
+  template: `<div
+    libBreakpointObserver
+    (breakpointChange)="onBreakpointChange($event)"
+  ></div>`,
 })
-class TestComponent {}
+class TestComponent {
+  onBreakpointChange(event: onBreakPointChangeCallbackArgs) {
+    //
+  }
+}
 
 describe('BreakpointObserverDirective', () => {
-  let fixture: ComponentFixture<TestComponent>;
   let directive: BreakpointObserverDirective;
-  let breakpointService: BreakpointService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [TestComponent, BreakpointObserverDirective],
-      providers: [
-        {
-          provide: BreakpointService,
-          useValue: {
-            getInstance: jasmine.createSpy('getInstance').and.returnValue({
-              activeBreakpoint: 'mockActiveBreakpoint',
-              breakpoints: 'mockBreakpoints',
-              on: jasmine.createSpy('on'),
-            }),
-          },
-        },
-      ],
+      providers: [BreakpointObserverDirective],
     });
 
-    fixture = TestBed.createComponent(TestComponent);
-    directive = fixture.debugElement
-      .query(By.directive(BreakpointObserverDirective))
-      .injector.get(BreakpointObserverDirective);
-    breakpointService = TestBed.inject(BreakpointService);
+    directive = TestBed.inject(BreakpointObserverDirective);
   });
 
-  it('should create an instance', () => {
-    expect(directive).toBeTruthy();
-  });
-
-  it('should initialize correctly', () => {
+  it('should call onBreakPointChange on ngOnInit', () => {
+    
     directive.ngOnInit();
-    expect(breakpointService.getInstance).toHaveBeenCalledWith(
-      directive.libBreakpointObserver
-    );
-    expect(directive.activeBreakpoint).toBe('mockActiveBreakpoint');
-    expect(directive.breakpointsList).toBe('mockBreakpoints');
-    expect(breakpointService.on).toHaveBeenCalledWith(
-      'onBreakPointChange',
-      jasmine.any(Function)
-    );
+
+    expect(directive.activeBreakpoint).toEqual({
+      breakpoint: 'sm',
+      maxWidth: 599,
+      minWidth: 0,
+    });
+    expect(directive.breakpointsList).toEqual({
+      sm: { min: 0, max: 599 },
+      md: { min: 600, max: 959 },
+      lg: { min: 960, max: 1279 },
+      xl: { min: 1280, max: Infinity },
+    });
   });
 
-  it('should handle onBreakPointChange correctly', () => {
-    const mockArgs: onBreakPointChangeCallbackArgs = [
-      'newActiveBreakpoint',
-      'newBreakpointsList',
+  it('should emit breakpointChange event on breakpoint change', () => {
+    directive.ngOnInit();
+    const breakpointService = BreakpointService.getInstance();
+    const spy = jest.spyOn(directive.breakpointChange, 'emit');
+
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    directive.breakpointService.updateBreakpoint('md');
+
+    const args: onBreakPointChangeCallbackArgs = [
+      { breakpoint: 'md', minWidth: 600, maxWidth: 959 },
+      {
+        sm: { min: 0, max: 599 },
+        md: { min: 600, max: 959 },
+        lg: { min: 960, max: 1279 },
+        xl: { min: 1280, max: Infinity },
+      }
     ];
-    spyOn(directive.breakpointChange, 'emit');
 
-    directive.onBreakPointChange(...mockArgs);
-
-    expect(directive.activeBreakpoint).toBe('newActiveBreakpoint');
-    expect(directive.breakpointsList).toBe('newBreakpointsList');
-    expect(directive.breakpointChange.emit).toHaveBeenCalledWith(mockArgs);
+    expect(spy).toHaveBeenCalledWith(args);
+    expect(directive.activeBreakpoint).toEqual(args[0]);
   });
 });
--- packages/react/src/lib/react.spec.tsx
+++ packages/react/src/lib/react.spec.tsx
@@ -1,10 +1,90 @@
-import { render } from '@testing-library/react';
+/* eslint-disable @typescript-eslint/ban-types */
+// import '@testing-library/jest-dom/extend-expect';
+import { BreakpointService } from '@breakpoint-observer/core';
+import { act, render, screen, waitFor } from '@testing-library/react';
+import { withBreakpointObserver } from './react';
 
-import React from './react';
+// Mock BreakpointService
+jest.mock('@breakpoint-observer/core', () => ({
+  BreakpointService: class {
+    public breakpoints = {
+      sm: { min: 0, max: 599 },
+      md: { min: 600, max: 959 },
+      lg: { min: 960, max: 1279 },
+      xl: { min: 1280, max: Infinity },
+    };
 
-describe('React', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(<React />);
-    expect(baseElement).toBeTruthy();
+    public activeBreakpoint = {
+      breakpoint: 'sm',
+      maxWidth: 599,
+      minWidth: 0,
+    };
+
+    constructor() {
+      //
+    }
+
+    private listeners: { [key: string]: Function[] } = {};
+
+    on(event: string, listener: Function) {
+      if (!this.listeners[event]) {
+        this.listeners[event] = [];
+      }
+      this.listeners[event].push(listener);
+      this.emit(event, this.activeBreakpoint);
+    }
+
+    updateBreakpoint(breakpoint: keyof typeof this.breakpoints) {
+      this.activeBreakpoint = {
+        breakpoint,
+        maxWidth: this.breakpoints[breakpoint].max,
+        minWidth: this.breakpoints[breakpoint].min,
+      };
+      this.emit('onBreakPointChange', this.activeBreakpoint);
+    }
+
+    emit(event: string, ...args: any[]) {
+      if (this.listeners[event]) {
+        this.listeners[event].forEach((listener) => listener(...args));
+      }
+    }
+
+    static getInstance() {
+      return new this();
+    }
+  },
+}));
+
+// Wrapped Component
+const WrappedComponent = withBreakpointObserver(({ activeBreakpoint }) => (
+  <div data-testid="mock-component">
+    {JSON.stringify(activeBreakpoint) ?? null}
+  </div>
+));
+
+describe('withBreakpointObserver HOC', () => {
+  it('should pass the initial breakpoint to the wrapped component', () => {
+    render(<WrappedComponent />);
+    expect(screen.getByTestId('mock-component').innerHTML).toBe(
+      JSON.stringify({ breakpoint: 'sm', maxWidth: 599, minWidth: 0 })
+    );
   });
+
+  it('should update the wrapped component when the breakpoint changes', async () => {
+    const breakpointService = BreakpointService.getInstance();
+
+    act(() => {
+      render(<WrappedComponent />);
+    });
+
+    // await new Promise((resolve) => setTimeout(resolve, 1000));
+    waitFor(() => {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-expect-error
+      breakpointService.updateBreakpoint('lg');
+      expect(screen.getByTestId('mock-component').innerHTML).toBe(
+        JSON.stringify({ breakpoint: 'lg', maxWidth: Infinity, minWidth: 960 })
+      );
+    });
+  }, 20000);
 });
